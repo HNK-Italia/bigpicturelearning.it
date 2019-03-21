d5dac53e6bad68e84f58a090ed0d8ceef5fb0a51
Walter <francesco.sasso@hnk.school>
2019-03-21 18:20:26 +0100

    Sistema il footer cone da progetto

--- dist/inc/footer.php
+++ dist/inc/footer.php
@@ -1,6 +1,35 @@
-<footer class="container">
-    <hr class="spacer py-2 py-md-2" />
-    <img class="rounded float-left rounded-0" src="bpl_italia_logo.png" width="42" height="42" alt="Immagine BPL" />
-    <p class="text-center"><a href="#">Privacy</a> · <a href="#">Terms</a></p>
-    <hr class="spacer py-1 py-md-2" />
-</footer>
+<container class="container-fluid">
+    <div class="infondo">
+      <hr class="spacer py-2 py-md-2" />
+      <div class="container">
+      <div class="row">
+        <div class="col-1">
+          <img src="bpl_italia_logo.png" width="75" height="75" />
+          <div></div>
+        </div>
+        <div class="col-6">
+          <p>SEDE LEGALE</p>
+          <p>Via G.B. Serralunga, 27</p>
+          <p>13900 - Biella (Italia)</p>
+        </div>
+        <div class="col-5">
+          <p>SEDE OPERATIVA</p>
+          <p>c/o SELLALAB</p>
+          <p>Via Corradino Sella, 10</p>
+          <p>13900 - Biella (Italia)</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-7">
+          <p>© 2019 All rights reserved Big Picture Learning Italia | P.I. 02579780020 </p>
+        </div>
+        <div class="col-5">
+          <a type="button" class="btn btn-dark" href="faq.php">
+            <p>Privacy Policy</p>
+          </a>
+        </div>
+      </div>
+      </div>
+      <hr class="spacer py-1 py-md-2" />
+    </div>
+</container>
